75db06e15b3fa8cef9059bcb4346842630e070e7
SimonLogan1 <sdlogan01@bvsd.org>
2024-09-10 21:53:58 +0000

    unfinished

--- IntegerArrayList.java
+++ IntegerArrayList.java
@@ -1,2 +1,21 @@
-public class IntegerArrayList extends IntegerList{
+public class IntegerArrayList implements IntegerList{
+    private int[] data;
+    private int size;
+
+    public IntegerArrayList()
+    {
+        size = 0;
+
+        data = new int[10];
+    }
+
+    public void add(Integer val)
+    {
+
+    }
+    public void add(int index, Integer val)
+    {
+
+    }
+    public void set(int index, Integer val)
 }
